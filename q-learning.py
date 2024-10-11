a83eb41086a59492030671a9be51eddad45ff263
Pizzaman9988 <supershooter@live.no>
2024-10-11 22:37:02 +0200

    Finished project

--- q-learning.py
+++ q-learning.py
@@ -1,5 +1,3 @@
-import sys
-import time
 import pygame
 from pygame.locals import *
 from robot import Robot
@@ -10,21 +8,36 @@ BLACK_COLOR = pygame.Color(0, 0, 0)
 WHITE_COLOR = pygame.Color(255, 255, 255)
 
 if __name__ == "__main__":
+    # Calls related to Q-learning.  
+    robot = Robot(
+        alpha=0.1, gamma=0.8, epsilon=1, min_epsilon=0.05, decay_rate=0.995
+    )  # Create a new robot.
+    robot.q_learning(epochs=1000)
+    robot.get_best_path()
+
+    # Result printing.
+    print(f"Best Score: {robot.best_score}\nBest Path:" , end=" ")
+    for state in robot.best_path:
+        if state == (5, 0):
+            print(state, end="")
+        else:
+            print(state, end=" -> ")
+
     pygame.init()
     fps_clock = pygame.time.Clock()
 
     play_surface = pygame.display.set_mode((500, 500))
     pygame.display.set_caption('Karaktersatt Oppgave 1 DTE2602')
-    simulator_speed = 10 # Adjust this value to change the speed of the visualization. Bigger number = more faster...
+    simulator_speed = 2  # Higher number == faster simulation.
 
-    bg_image = pygame.image.load("grid.jpg") # Loads the simplified grid image.
-    #bg_image = pygame.image.load("map.jpg") # Uncomment this to load the terrain map image.
+    bg_image = pygame.image.load("grid.jpg")  # Loads simplified grid image.
+    #bg_image = pygame.image.load("map.jpg")  # Loads terrain map image.
 
-    robot = Robot() # Create a new robot.
-    robot.reset_random()
-
-    # Pygame boilerplate code.
+    # Visualize best path.
     running = True
+    i = 0
+    robot.y = 0
+    robot.x = 3
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -40,15 +53,23 @@ if __name__ == "__main__":
         play_surface.fill(WHITE_COLOR) # Fill the screen with white.
         play_surface.blit(bg_image, (0, 0)) # Render the background image.
 
-        # Render the robot over the image.
-        pygame.draw.rect(play_surface, BLACK_COLOR, Rect(robot.x * 70 + 69, robot.y * 70 + 69, 22, 22)) # A black outline.
-        pygame.draw.rect(play_surface, GREEN_COLOR, Rect(robot.x * 70 + 70, robot.y * 70 + 70, 20, 20)) # The robot is rendered in green, you may change this if you want.
-
-        # Calls related to Q-learning.
+        # Best path iteration
         if robot.has_reached_goal():
-            robot.reset_random()
+            pass
         else:
-            robot.one_step_q_learning()
+            robot.y = robot.best_path[i][0]
+            robot.x = robot.best_path[i][1]
+        i += 1
+
+        # Render the robot over the image.
+        pygame.draw.rect(
+            play_surface, BLACK_COLOR,
+            Rect(robot.x * 70 + 69,robot.y * 70 + 69, 22, 22)
+        )  # A black outline around the robot.
+        pygame.draw.rect(
+            play_surface, GREEN_COLOR,
+            Rect(robot.x * 70 + 70, robot.y * 70 + 70, 20, 20)
+        )  # The robot is rendered in green.
 
         # Refresh the screen.
         pygame.display.flip()
--- robot.py
+++ robot.py
@@ -1,56 +1,138 @@
 import random as rnd
 
 class Robot:
+    """
+    Class object for machine learning agent, a theoretical robot.
+    
+    Attributes
+    ----------
+    alpha: float
+        Learning rate, [0, 1]
+    gamma: float
+        Discount factor, [0, 1]
+    epsilon: float
+        Chance of doing exploration insted of exploitation [0, 1]
+    min_epsilon: float
+        Minimum value for epsilon [0, 1]
+    decay_rate: float
+        How fast epsilon decreases per epoch [0, 1]
+    best_path: list of tuples
+        Chronological list of states visited when using best found path.
+    best_score: int
+        Score achieved using best found path.
 
-    def __init__(self):
+    Methods
+    -------
+    set_next_state(action: int) -> int:
+        Updates robot position based on action (up, down, right, left)
+        Checks if action is legal and returns new state index.
+    get_action_greedy(state: int) -> int:
+        Returns best action based on greedy policy, which is the action
+        with the highest Q-value for current state. If multiple of same
+        value selects one of them randomly.
+    get_action_mc() -> int:
+        Randomly selects and returns an action (0-3), simulating Monte
+        Carlo policy.
+    get_action_eg(state) -> int:
+        Returns get_action_mc() if random number < epsilon, else
+        returns get_action_greedy().
+    epsilon_update():
+        Decreases epsilon by factor decay_rate to a minimum value of
+        min_epsilon.
+    monte_carlo_exploration(epochs: int) ->:
+        Explores terrain with Monte Carlo policy, with equal chance of
+        choosing each action. Starts in (0, 3), resets in (5, 0).
+        Repeats for given number of epochs, then prints best score
+        achieved.
+    one_step_q_learning():
+        Executes one step of q-learning. Updates state and q-matrix.
+    q_learning(epochs: int):
+        Runs q_learning for given number of epochs. Starts each epoch
+        in random state, ends epoch when reaching (5, 0). Reduces
+        epsilon for each epoch.
+    has_reached_goal():
+        Returns True if target state == (5, 0) is reached.
+    reset_random():
+        Sets x and y coordinates to random values in [0, 5].
+    get_best_path():
+        Finds best path by using a greedy policy to pick the most
+        rewarding path according to the q-matrix. Also records
+        highest score.
+    """
+
+    def __init__(
+            self, alpha: float, gamma: float, epsilon: float,
+            min_epsilon: float, decay_rate: float
+        ):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.min_epsilon = min_epsilon
+        self.decay_rate = decay_rate
+        self.best_path = []
+        self.best_score = 0
         self.y = None
         self.x = None
-        # Define R- and Q-matrices here.
-                        #Actions
-                        #  Up  Down Right Left  # States
-        self.r_matrix = [[-100, -15, -5, -100], # (0,0)
-                        [-100, -15, -5, -15],   # (0,1)
-                        [-100, -1, -1, -5],     # (0,2)
-                        [-100, -5, -1, -5],     # (0,3)
-                        [-100, -1, -15, -1],    # (0,4)
-                        [-100, -1, -100, -1],   # (0,5)
-                        [-15, -5, -15, -100],   # (1,0)
-                        [-5, -1, -1, -15],      # (1,1)
-                        [-5, -1, -5, -15],      # (1,2)
-                        [-1, -5, -1, -1],       # (1,3)
-                        [-1, -1, -1, -5],       # (1,4)
-                        [-15, -5, -100, -1],    # (1,5)
-                        [-15, -5, -1, -100],    # (2,0)
-                        [-15, -1, -1, -5],      # (2,1)
-                        [-1, -1, -5, -1],       # (2,2)
-                        [-5, -1, -1, -1],       # (2,3)
-                        [-1, -1, -5, -5],       # (2,4)
-                        [-1, -1, -100, -1],     # (2,5)
-                        [-5, -5, -1, -100],     # (3,0)
-                        [-1, -1, -1, -5],       # (3,1)
-                        [-1, -5, -1, -1],       # (3,2)
-                        [-5, -1, -1, -1],       # (3,3)
-                        [-1, -5, -1, -1],       # (3,4)
-                        [-5, -1, -100, -1],     # (3,5)
-                        [-5, 50, -1, -100],     # (4,0)
-                        [-1, -15, -5, -5],      # (4,1)
-                        [-1, -15, -1, -1],      # (4,2)
-                        [-1, -15, -5, -5],      # (4,3)
-                        [-1, -15, -1, -1],      # (4,4)
-                        [-1, -15, -100, -5],    # (4,5)
-                        [-5, -100, -15, -100],  # (5,0)
-                        [-1, -100, -15, 50],    # (5,1)
-                        [-5, -100, -15, -15],   # (5,2)
-                        [-1, -100, -15, -15],   # (5,3)
-                        [-5, -100, -15, -15],   # (5,4)
-                        [-1, -100, -100, -15]]  # (5,5)
+            # Actions
+            # Up  Down Right Left  # States
+        self.r_matrix = [
+            [-100, -15, -5, -100],  # (0,0)
+            [-100, -15, -5, -15],   # (0,1)
+            [-100, -1, -1, -5],     # (0,2)
+            [-100, -5, -1, -5],     # (0,3)
+            [-100, -1, -15, -1],    # (0,4)
+            [-100, -1, -100, -1],   # (0,5)
+            [-15, -5, -15, -100],   # (1,0)
+            [-5, -1, -1, -15],      # (1,1)
+            [-5, -1, -5, -15],      # (1,2)
+            [-1, -5, -1, -1],       # (1,3)
+            [-1, -1, -1, -5],       # (1,4)
+            [-15, -5, -100, -1],    # (1,5)
+            [-15, -5, -1, -100],    # (2,0)
+            [-15, -1, -1, -5],      # (2,1)
+            [-1, -1, -5, -1],       # (2,2)
+            [-5, -1, -1, -1],       # (2,3)
+            [-1, -1, -5, -5],       # (2,4)
+            [-1, -1, -100, -1],     # (2,5)
+            [-5, -5, -1, -100],     # (3,0)
+            [-1, -1, -1, -5],       # (3,1)
+            [-1, -5, -1, -1],       # (3,2)
+            [-5, -1, -1, -1],       # (3,3)
+            [-1, -5, -1, -1],       # (3,4)
+            [-5, -1, -100, -1],     # (3,5)
+            [-5, 50, -1, -100],     # (4,0)
+            [-1, -15, -5, -5],      # (4,1)
+            [-1, -15, -1, -1],      # (4,2)
+            [-1, -15, -5, -5],      # (4,3)
+            [-1, -15, -1, -1],      # (4,4)
+            [-1, -15, -100, -5],    # (4,5)
+            [-5, -100, -15, -100],  # (5,0)
+            [-1, -100, -15, 50],    # (5,1)
+            [-5, -100, -15, -15],   # (5,2)
+            [-1, -100, -15, -15],   # (5,3)
+            [-5, -100, -15, -15],   # (5,4)
+            [-1, -100, -100, -15]   # (5,5)
+        ]
         self.q_matrix = [[0] * 4 for _ in range(36)]
 
-    def get_action_mc(self):
-        return rnd.randint(0, 3)
+    def set_next_state(self, action: int) -> int:
+        """
+        Change and return position based on action, if action is legal.
 
-    def set_next_state_mc(self, action):
-        # Return the next state based on Monte Carlo.
+        Parameters
+        ----------
+        action: int
+            The action to perform, where:
+            - 0: Move up (decrement y, if y > 0)
+            - 1: Move down (increment y, if y < 5)
+            - 2: Move right (increment x, if x < 5)
+            - 3: Move left (decrement x, if x > 0)
+
+        Returns
+        -------
+        int
+            Row number in r/q-matrix.
+        """
         match action:
             case 0:
                 if self.y != 0:
@@ -64,14 +146,73 @@ class Robot:
             case 3:
                 if self.x != 0:
                     self.x -= 1
-        return
+        return self.y * 6 + self.x
 
-    def get_next_state_eg(self):
-        # Return the next state based on Epsilon-greedy.
-        pass
+    def get_action_greedy(self, state: int) -> int:
+        """
+        Return best action for current state based on greedy policy.
 
-    def monte_carlo_exploration(self, epochs):
-        score = []
+        Parameters
+        ----------
+        state: int
+            Row number in r/q-matrix.
+
+        Returns
+        -------
+        int
+            Highest rewarding action, between 0 and 3.
+        """
+        best_q = max(self.q_matrix[state])
+        best_actions = ([
+            index for index, value in 
+            enumerate(self.q_matrix[state]) if value == best_q
+        ])
+        return rnd.choice(best_actions)
+    
+    def get_action_mc(self) -> int:
+        """Return int representing action based on Monte Carlo policy"""
+
+        return rnd.randint(0, 3)
+
+    def get_action_eg(self, state: int) -> int:
+        """
+        Returns Monte Carlo or greedy action based on epsilon
+
+        Parameters
+        ----------
+        state: int
+            Row number in r/q-matrix
+
+        Returns
+        -------
+        int
+            Action between 0 and 3.
+        """
+        if rnd.uniform(0, 1) < self.epsilon:
+            return self.get_action_mc()
+        else:
+            return self.get_action_greedy(state)
+        
+    def epsilon_update(self) -> None:
+        """Changes epsilon by decay_rate until it is min_epsilon."""
+
+        self.epsilon = max(self.min_epsilon, self.epsilon * self.decay_rate)
+        return None
+
+    def monte_carlo_exploration(self, epochs: int) -> None:
+        """
+        Prints the highest score achieved in given number of epochs.
+
+        Parameters
+        ----------
+        epochs: int
+            Number of simulation iterations to perform
+
+        Returns
+        -------
+        None
+        """
+        scores = []
         for _ in range(epochs):
             self.y = 0
             self.x = 3
@@ -80,38 +221,64 @@ class Robot:
             while not finished:
                 action = self.get_action_mc()
                 sum_reward += self.r_matrix[6 * self.y + self.x][action]
-                self.set_next_state_mc(action)
+                self.set_next_state(action)
                 finished = self.has_reached_goal()
-            score.append(sum_reward)
-        print(max(score))
+            scores.append(sum_reward)
+        print(max(scores))
+        return None
 
+    def one_step_q_learning(self) -> None:
+        """Execute one step of q-learning."""
 
-    def q_learning(self):
-        pass
-        
-    def one_step_q_learning(self):
+        current_state = self.y * 6 + self.x
+        action = self.get_action_eg(current_state)
+        next_state = self.set_next_state(action)
+        reward = self.r_matrix[current_state][action]
+        self.q_matrix[current_state][action] = (
+            (1 - self.alpha) * self.q_matrix[current_state][action] + self.alpha
+            * (reward + self.gamma * max(self.q_matrix[next_state]))
+        )
+        return None
 
-        # Get action based on policy
-        # Get the next state based on the action
-        # Get the reward for going to this state
-        # Update the Q-matrix
-        # Go to the next state
-        pass
-    
+    def q_learning(self, epochs: int) -> None:
+        """Execute q-learning for given number of epochs"""
+
+        for _ in range(epochs):
+            self.reset_random()
+            while not self.has_reached_goal():
+                self.one_step_q_learning()
+            self.epsilon_update()
+        return None
+
     def has_reached_goal(self):
+        """Return True if Robot has reached target destination (5, 0)"""
+
         if self.y == 5 and self.x == 0:
             return True
         else:
             return False
         
     def reset_random(self):
+        """Sets robot position to a random grid location"""
+
         self.y = rnd.randint(0, 5)
         self.x = rnd.randint(0, 5)
-        return
+        return None
 
-    def greedy_path(self):
-        pass
+    def get_best_path(self) -> None:
+        """Finds and saves best path and its score."""
 
-robot = Robot()
+        self.y = 0
+        self.x = 3
+        self.best_path.append((self.y, self.x))
+        while not self.has_reached_goal():
+            current_state = self.y * 6 + self.x
+            action = self.get_action_greedy(current_state)
+            self.set_next_state(action)
+            self.best_score += self.r_matrix[current_state][action]
+            self.best_path.append((self.y, self.x))
+        return None
 
-robot.monte_carlo_exploration(10000)
+#For testing monte carlo exploration
+#robot = Robot()
+#robot.monte_carlo_exploration(10000)
